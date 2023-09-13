36feb5d838ff1210583620a030bfc718cb41a06c
jurazil <jurazilgreen@gmail.com>
2023-09-12 23:59:18 -0700

    removed flexible widget from dropdown column

--- lib/core/uploaded/dropdown.dart
+++ lib/core/uploaded/dropdown.dart
@@ -25,41 +25,39 @@ class CustomDropdownState extends State<CustomDropdown> {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onTap: toggleDropdown,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        GestureDetector(
+          onTap: toggleDropdown,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Icon(
-                    isDropdownOpen
-                        ? Icons.arrow_drop_down
-                        : Icons.arrow_drop_up,
-                  ),
-                ],
-              ),
+                ),
+                Icon(
+                  isDropdownOpen ? Icons.arrow_drop_down : Icons.arrow_drop_up,
+                ),
+              ],
             ),
           ),
-          if (isDropdownOpen)
-            Flexible(
-              child: widget.child,
-            ),
-        ],
-      ),
+        ),
+        if (isDropdownOpen)
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+            width: double.infinity,
+            child: widget.child,
+          ),
+      ],
     );
   }
 }
